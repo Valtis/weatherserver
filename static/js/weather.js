f7f50281908f69ebab5490bc823de657ebab8643
Erkka Kääriä <erkka.kaaria@outlook.com>
2020-07-16 12:56:24 +0300

    Alter layout, display high/low valuesw for last 24h and overall period

--- static/js/weather.js
+++ static/js/weather.js
@@ -13,16 +13,29 @@ async function fetch_data(url, start, end) {
 }
 
 async function drawTemperatureGraph() {
-    await drawGraph("temperature", "Lämpötila")
+    let values = await drawGraph("temperature", "Lämpötila", "C");
+    updateLastMeasurementTime(values);
+
+    let midnight = new Date();
+    midnight.setHours(0, 0, 0, 0);
+
+    updateMinMaxMeasurement(values, "temperature", "C", midnight);
+    updateMinMaxMeasurement(values, "temperature-all-time", "C");
 }
 
 
 async function drawHumidityGraph() {
-    await drawGraph("humidity", "Ilmankosteus")
+    let values = await drawGraph("humidity", "Ilmankosteus", "%");
+
+    let midnight = new Date();
+    midnight.setHours(0, 0, 0, 0);
+
+    updateMinMaxMeasurement(values, "humidity", "%", midnight);
+    updateMinMaxMeasurement(values, "humidity-all-time", "%");
 }
 
 
-async function drawGraph(graphType, title) {
+async function drawGraph(graphType, title, lastMeasurementUnit) {
     let datePickerStart = graphType + "-start";
     let datePickerEnd = graphType + "-end";
     start = document.getElementById(datePickerStart).value;
@@ -53,7 +66,7 @@ async function drawGraph(graphType, title) {
     let time = graphData.map(item => new Date(item["logged_at"]));
     let measuredValues = graphData.map(item => item["measured_" + graphType]);
 
-    document.getElementById("last-" + graphType).innerHTML = parseFloat(measuredValues[measuredValues.length-1]).toFixed(1);
+    document.getElementById("last-" + graphType).innerHTML = parseFloat(measuredValues[measuredValues.length-1]).toFixed(1) + lastMeasurementUnit;
 
     var dataMap = {
         x: time,
@@ -77,5 +90,46 @@ async function drawGraph(graphType, title) {
     };
 
     Plotly.newPlot(graphType, data, layout, config);
+
+    return {
+        values: measuredValues,
+        times: time
+    };
+}
+
+function updateLastMeasurementTime(values) {
+    let latestTime = values.times[values.times.length - 1];
+    let elem = document.getElementById("last-measurement-time").innerHTML = latestTime.toLocaleString();
+}
+
+function updateMinMaxMeasurement(values, type, unit, startDate) {
+    let startIndex = 0;
+    if (startDate != undefined) {
+        while (startDate > values.times[++startIndex] && startIndex < values.times.length);
+
+        if (startIndex >= values.times.length) {
+            return;
+        }
+    }
+
+    let maxIndex = startIndex;
+    let minIndex = startIndex;
+
+    for (let i = startIndex; i < values.times.length; ++i) {
+        if (values.values[i] > values.values[maxIndex]) {
+            maxIndex = i;
+        }
+
+        if (values.values[i] < values.values[minIndex]) {
+            minIndex = i;
+        }
+    }
+
+
+    document.getElementById("highest-"+type).innerHTML =
+        parseFloat(values.values[maxIndex]).toFixed(1) + unit + ", mitattu " + values.times[maxIndex].toLocaleString();
+
+    document.getElementById("lowest-"+type).innerHTML =
+        parseFloat(values.values[minIndex]).toFixed(1) + unit + ", mitattu " + values.times[minIndex].toLocaleString();
 }
 
